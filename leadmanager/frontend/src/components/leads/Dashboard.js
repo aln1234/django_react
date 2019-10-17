f417f1278a84b58c2dd970650169080ec096fe71
aln1234 <39218356+aln1234@users.noreply.github.com>
2019-10-17 17:52:55 +0545

    Add files via upload

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Form from './Form';
+import Lead from './Lead';
+
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Lead />
+        </Fragment>
+    )
+}
---
+++ leadmanager/frontend/src/components/leads/Lead.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Lead extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add leads list</h1>
+            </div>
+        )
+    }
+}
+
+export default Lead
